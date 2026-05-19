03617ae2a5385541410b0dfa39105d404f5df692
Oskar Majcen <leagueofdisynk@gmail.com>
2026-05-19 15:37:23 +0200

    Changed the way keyboard presses are handled

--- platfinder.py
+++ platfinder.py
@@ -2,24 +2,34 @@ from colorama import Fore
 import psutil
 import keyboard
 import os
-import threading
+#import threading
 import time
 
+isRunning = False
+screenshottingEnabled = False
+
 def main():
+    global isRunning
     while(True):
         if "Warframe.x64.exe" in (i.name() for i in psutil.process_iter()):
+            isRunning = True
             print(Fore.GREEN + "Warframe is running!" + Fore.RESET)
         else:
+            isRunning = False
             print(Fore.RED + "Warframe is not running!" + Fore.RESET)
 
         time.sleep(2)
 
-def keyManager():
-    while True:
-        if keyboard.is_pressed("shift+F12"):
-            print(Fore.CYAN + "Program exited!" + Fore.RESET)
-            os._exit(1)
+def enableScreenshotting():
+    global isRunning
+    global screenshottingEnabled
+
+    if isRunning:
+        screenshottingEnabled = True
+        print(Fore.CYAN + "Screenshotting enabled!" + Fore.RESET)
         
+keyboard.add_hotkey("shift+f12", lambda: os._exit(1))
+keyboard.add_hotkey("shift+space", enableScreenshotting)
 
 print(Fore.BLUE + r"__________.__          __ ___________.__            .___            ") 
 print(Fore.BLUE + r"\______   \  | _____ _/  |\_   _____/|__| ____    __| _/___________ ") 
@@ -31,6 +41,4 @@ print(Fore.RESET)
 print(Fore.GREEN + "PlatFinder is running... \n" + Fore.RED + "Press shift+F12 to close the program")
 print(Fore.RESET)
 
-thread2 = threading.Thread(target=keyManager)
-thread2.start()
 main()
