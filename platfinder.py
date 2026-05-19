ab03416d4ba2af71d9cb0cfabf3865b3733cc3a6
Oskar Majcen <leagueofdisynk@gmail.com>
2026-05-19 14:21:07 +0200

    Added Warframe process detection and additional thread to handle keypresses

--- platfinder.py
+++ platfinder.py
@@ -1,10 +1,25 @@
 from colorama import Fore
+import psutil
+import keyboard
+import os
+import threading
 import time
 
 def main():
     while(True):
-        print('meow')
-        time.sleep(4)
+        if "Warframe.x64.exe" in (i.name() for i in psutil.process_iter()):
+            print(Fore.GREEN + "Warframe is running!" + Fore.RESET)
+        else:
+            print(Fore.RED + "Warframe is not running!" + Fore.RESET)
+
+        time.sleep(2)
+
+def keyManager():
+    while True:
+        if keyboard.is_pressed("shift+F12"):
+            print(Fore.CYAN + "Program exited!" + Fore.RESET)
+            os._exit(1)
+        
 
 print(Fore.BLUE + r"__________.__          __ ___________.__            .___            ") 
 print(Fore.BLUE + r"\______   \  | _____ _/  |\_   _____/|__| ____    __| _/___________ ") 
@@ -16,7 +31,6 @@ print(Fore.RESET)
 print(Fore.GREEN + "PlatFinder is running... \n" + Fore.RED + "Press shift+F12 to close the program")
 print(Fore.RESET)
 
+thread2 = threading.Thread(target=keyManager)
+thread2.start()
 main()
-
-
-        
