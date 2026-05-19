a1e942581efd454778b60af771bcfe5c69d0630e
Oskar Majcen <leagueofdisynk@gmail.com>
2026-05-19 13:38:05 +0200

    Initialization of project

---
+++ platfinder.py
@@ -0,0 +1,22 @@
+from colorama import Fore
+import time
+
+def main():
+    while(True):
+        print('meow')
+        time.sleep(4)
+
+print(Fore.BLUE + r"__________.__          __ ___________.__            .___            ") 
+print(Fore.BLUE + r"\______   \  | _____ _/  |\_   _____/|__| ____    __| _/___________ ") 
+print(Fore.BLUE + r" |     ___/  | \__  \\   __\    __)  |  |/    \  / __ |/ __ \_  __ \\") 
+print(Fore.BLUE + r" |    |   |  |__/ __ \|  | |     \   |  |   |  \/ /_/ \  ___/|  | \/") 
+print(Fore.BLUE + r" |____|   |____(____  /__| \___  /   |__|___|  /\____ |\___  >__|   ") 
+print(Fore.BLUE + r"                    \/         \/            \/      \/    \/       ")
+print(Fore.RESET)
+print(Fore.GREEN + "PlatFinder is running... \n" + Fore.RED + "Press shift+F12 to close the program")
+print(Fore.RESET)
+
+main()
+
+
+        
